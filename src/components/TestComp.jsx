2205f9bb00e85a8eae76982dd23c39d0290785de
Anish George <anishgeo@thoughtworks.com>
2023-06-08 14:50:50 +0530

    Add carbon components

--- src/components/TestComp.jsx
+++ src/components/TestComp.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './TestComp.scss';
+import { Switch, ContentSwitcher } from 'carbon-components-react';
 
 export function TestComponent() {
   return (
     <div className='test-comp'>
       <h3>Hello World!</h3>
+      <ContentSwitcher>
+        <Switch name="one" >Hello</Switch>
+        <Switch name="two" text="Two" />
+      </ContentSwitcher>
     </div>
   )
 }
