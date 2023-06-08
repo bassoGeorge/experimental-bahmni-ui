1c0bf96660cc3ba29ee0258d0e1b41cfc70e0546
Anish George <anishgeo@thoughtworks.com>
2023-06-08 09:46:26 +0530

    Add storybook

---
+++ src/components/TestComp.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function TestComponent() {
+  return (
+    <div>
+      <h3>Hello World!</h3>
+    </div>
+  )
+}
---
+++ src/components/TestComp.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { TestComponent } from './TestComp';
+
+export default {
+  title: 'TestComp',
+  component: TestComponent,
+}
+
+export const Primary = () => <TestComponent />;
