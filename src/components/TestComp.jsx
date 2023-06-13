0a9d3995fbb9cdc2e65738ccc57ce4d61be26bea
Anish George <anishgeo@thoughtworks.com>
2023-06-13 11:34:08 +0530

    Add lint-staged to elint and correct the npmrc problem

--- src/components/TestComp.jsx
+++ src/components/TestComp.jsx
@@ -3,9 +3,11 @@ import './TestComp.scss';
 import { Switch, ContentSwitcher } from 'carbon-components-react';
 
 export function TestComp() {
+  const n = "Random"
   return (
     <div className='test-comp'>
       <h3>Hello World!</h3>
+      <span>{n}</span>
       <ContentSwitcher>
         <Switch name="one" >Hello</Switch>
         <Switch name="two" text="Two" />
