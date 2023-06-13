227e72607ce19c24ecce52e09ca3f7eaf48abed7
Anish George <anishgeo@thoughtworks.com>
2023-06-13 10:52:12 +0530

    Configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  ignorePatterns: ["node_modules/*", "dist/*", ".storybook/*", ".*"],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {},
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  overrides: [
+    {
+      files: ["*.spec.js", "*.spec.jsx"],
+      plugins: ["jest"],
+      extends: ["plugin:jest/recommended"],
+      rules: {}
+    }
+  ]
+};
